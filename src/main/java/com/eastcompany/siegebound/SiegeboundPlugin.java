0302783f5fad3e705d7ce2311b3679885a2c6257
Kotaro <tanakajapanese39@gmail.com>
2025-11-03 18:32:20 +0900

    macos

--- src/main/java/com/eastcompany/siegebound/SiegeboundPlugin.java
+++ src/main/java/com/eastcompany/siegebound/SiegeboundPlugin.java
@@ -3,18 +3,32 @@ package com.eastcompany.siegebound;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.eastcompany.siegebound.command.SiegeboundCommand;
+import com.eastcompany.siegebound.manager.SiegeManager;
+
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 
 public class SiegeboundPlugin extends JavaPlugin{
 	
+	private static SiegeManager siegeManager;
 	
 	@Override
 	public void onEnable() {
-		
-		
-		
-		
-		Bukkit.broadcast(Component.text("Hello mother Fuker"));
+		//Config 呼び出し
+		Config.init(this);
+		//コマンド呼び出し
+		this.getCommand("siegebound").setExecutor(new SiegeboundCommand());
+		//有効化メッセージ
+		Bukkit.broadcast(Config.PREFIX.append(Component.text("シージバウンド有効化",NamedTextColor.WHITE)));
+	}
+	
+	public static SiegeManager getSiegeManager() {
+		return siegeManager;
+	}
+	
+	public static void createnewgame() {
+		siegeManager = new SiegeManager();
 	}
 
 }
