23b099c83cdba7ca12c96d3fb36faca29bfaf06a
Kotaro <tanakajapanese39@gmail.com>
2025-11-03 16:40:44 +0900

    Message

--- src/main/java/com/eastcompany/siegebound/SiegeboundPlugin.java
+++ src/main/java/com/eastcompany/siegebound/SiegeboundPlugin.java
@@ -1,11 +1,20 @@
 package com.eastcompany.siegebound;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import net.kyori.adventure.text.Component;
 
 public class SiegeboundPlugin extends JavaPlugin{
 	
+	
+	@Override
 	public void onEnable() {
 		
+		
+		
+		
+		Bukkit.broadcast(Component.text("Hello mother Fuker"));
 	}
 
 }
